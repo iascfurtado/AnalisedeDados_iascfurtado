b7849772291572e8e07796db9f18185d132ec5d4
ias <103458831+iascfurtado@users.noreply.github.com>
2022-05-25 14:35:24 -0300

    Rename SQLQuery2.sql to 0 SQLQuery2.sql

--- 0 SQLQuery2.sql
+++ 0 SQLQuery2.sql
@@ -11,7 +11,7 @@ SELECT TOP 10 * FROM product_category_name_translation --ok
 -- para nao precisar mostrar a tabela gigantesca
 -- apareceram problemas na tabela de carga onde na hora de carregar haviam
 -- dados que eram para ser datetime mas foram carregados como varchar, nesse caso
--- È necess·rio fazer um update nessas tabelas para atualizar esses campos para ai
+-- √© necess√°rio fazer um update nessas tabelas para atualizar esses campos para ai
 -- sim criar a tabela de producao, sendo
 
 UPDATE [dbo].[olist_orders_dataset] 
